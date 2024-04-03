2da73170ae293ecc4b4e3f9ef937b7dc55c1eafb
Lucas <lucas.vergara@live.com>
2024-04-02 23:16:04 -0300

    :construction: Create Fetch mechanism to get jokes from api and show in the screen

--- app.js
+++ app.js
@@ -3,8 +3,21 @@ console.log('Dad Jokes Starter');
 const url = 'https://icanhazdadjoke.com/';
 
 const btn = document.querySelector('.btn')
-const result = document.querySelector('.result');
+const result = document.querySelector('.result-text-part');
 
 btn.addEventListener('click', () => {
-    console.log('click test');
+    fetchDadJoke();
 });
+
+const fetchDadJoke = async() =>{
+    const response = await fetch(url,{
+        headers:{
+            Accept:'application/json',
+            'User-Agent':'learning app',
+        },
+    });
+    const data = await response.json();
+    result.textContent = data.joke;
+};
+
+fetchDadJoke();
